0a8b7904c00a2c65552759cee76d2ed5778bc7ce
Wasim Nazir <quic_wasimn@quicinc.com>
2024-10-04 14:38:59 +0530

    FROMLIST: soc: qcom: socinfo: add QCS9075 SoC ID
    Update soc_id table for the Qualcomm QCS9075 SoC
    to represent qcs9075 machine.
    
    Change-Id: I179c5cefe6332f0e041c50e99aaa81c8f22f3f06
    Link: https://lore.kernel.org/all/20241110145339.3635437-3-quic_wasimn@quicinc.com/
    Patch-mainline: linux-arm-msm @ 10/11/2024, 20:23
    Signed-off-by: Wasim Nazir <quic_wasimn@quicinc.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -438,6 +438,7 @@ static const struct soc_id soc_id[] = {
 	{ qcom_board_id(QCS9100) },
 	{ qcom_board_id(QCS8300) },
 	{ qcom_board_id(QCS8275) },
+	{ qcom_board_id(QCS9075) },
 	{ qcom_board_id(QAM8397P) },
 	{ qcom_board_id(QAM8797P) },
 };
