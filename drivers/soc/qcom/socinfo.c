604078c2b8d4a5a53ea0649d6bebb618b627e210
Yadu MG <quic_ymg@quicinc.com>
2024-10-30 18:26:02 +0530

    PENDING: soc: qcom: socinfo: add support for SA8797P SoC
    Add SocInfo support for SA8797P,QAM8797P and QAM8397P SoC.
    
    Change-Id: Ic773b675dfcb2134e0da0c64230a026bc28e3af5
    Signed-off-by: Yadu MG <quic_ymg@quicinc.com>

--- drivers/soc/qcom/socinfo.c
+++ drivers/soc/qcom/socinfo.c
@@ -434,9 +434,12 @@ static const struct soc_id soc_id[] = {
 	{ qcom_board_id(QCM8550) },
 	{ qcom_board_id(SA7255P) },
 	{ qcom_board_id(IPQ5300) },
+	{ qcom_board_id(SA8797P) },
 	{ qcom_board_id(QCS9100) },
 	{ qcom_board_id(QCS8300) },
 	{ qcom_board_id(QCS8275) },
+	{ qcom_board_id(QAM8397P) },
+	{ qcom_board_id(QAM8797P) },
 };
 
 static const char *socinfo_machine(struct device *dev, unsigned int id)
