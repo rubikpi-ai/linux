8cba777427501a0a257c69d95e63f4865b59698c
Paloma Arellano <quic_parellan@quicinc.com>
2024-10-31 15:08:31 +0800

    UPSTREAM: drm/dp: add an API to indicate if sink supports VSC SDP
    YUV420 format is supported only in the VSC SDP packet and not through
    MSA. Hence add an API which indicates the sink support which can be used
    by the rest of the DP programming.
    
    changes in v5:
    	- rebased on top of drm-tip
    
    changes in v4:
    	- bail out early if dpcd rev check fails
    
    changes in v3:
    	- fix the commit title prefix to drm/dp
    	- get rid of redundant git add -u
    	- break out this change from series [1] to get acks from drm core
    	  maintainers
    
    Changes in v2:
    	- Move VSC SDP support check API from dp_panel.c to
    	  drm_dp_helper.c
    
    [1]: https://patchwork.freedesktop.org/series/129180/
    
    Change-Id: I6450a9119f6740a5ee711e15dbdcf4b833deb928
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Paloma Arellano <quic_parellan@quicinc.com>
    Signed-off-by: Abhinav Kumar <quic_abhinavk@quicinc.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240215191556.3227259-1-quic_abhinavk@quicinc.com
    Git-Commit: 0d024974014f39207c5f52e770059b5bac35ea6d
    Git-repo: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Yongxing Mou <quic_yongmou@quicinc.com>

--- drivers/gpu/drm/display/drm_dp_helper.c
+++ drivers/gpu/drm/display/drm_dp_helper.c
@@ -2917,6 +2917,29 @@ void drm_dp_vsc_sdp_log(const char *level, struct device *dev,
 EXPORT_SYMBOL(drm_dp_vsc_sdp_log);
 
 /**
+ * drm_dp_vsc_sdp_supported() - check if vsc sdp is supported
+ * @aux: DisplayPort AUX channel
+ * @dpcd: DisplayPort configuration data
+ *
+ * Returns true if vsc sdp is supported, else returns false
+ */
+bool drm_dp_vsc_sdp_supported(struct drm_dp_aux *aux, const u8 dpcd[DP_RECEIVER_CAP_SIZE])
+{
+	u8 rx_feature;
+
+	if (dpcd[DP_DPCD_REV] < DP_DPCD_REV_13)
+		return false;
+
+	if (drm_dp_dpcd_readb(aux, DP_DPRX_FEATURE_ENUMERATION_LIST, &rx_feature) != 1) {
+		drm_dbg_dp(aux->drm_dev, "failed to read DP_DPRX_FEATURE_ENUMERATION_LIST\n");
+		return false;
+	}
+
+	return (rx_feature & DP_VSC_SDP_EXT_FOR_COLORIMETRY_SUPPORTED);
+}
+EXPORT_SYMBOL(drm_dp_vsc_sdp_supported);
+
+/**
  * drm_dp_get_pcon_max_frl_bw() - maximum frl supported by PCON
  * @dpcd: DisplayPort configuration data
  * @port_cap: port capabilities
